8bdc920799b0bc1fa5d7399cb9f976a225b98973
JB Bailly <jean.baptiste.bailly@gmail.com>
2024-08-20 11:48:37 +0200

    format and move validate All location script

--- resources/validateAllLocations.js
+++ resources/validateAllLocations.js
@@ -13,15 +13,20 @@ const oa = new OaSdk({
 });
 
 
+/**
+ * Validates all locations (status =1 ) in the OpenAgenda instance that are in the "to be validated" state.
+ *
+ * @return {Promise<void>} Resolves when all locations have been validated.
+ */
 const validateLocations = async () => {
   oa.connect();
   const locations = await oa.locations.list(AGENDA_UID, { state: 0 });
   if (locations && locations.length > 1) {
-      for (const location of locations.locations) {
-        await oa.locations.patch(AGENDA_UID, location.uid, { state: 1 });
-        console.log("Validated location: ", location.name)
-      }
-      console.log("Validated locations: ", locations.length)
+    for (const location of locations.locations) {
+      await oa.locations.patch(AGENDA_UID, location.uid, { state: 1 });
+      console.log("Validated location: ", location.name)
+    }
+    console.log("Validated locations: ", locations.length)
   } else {
     console.log("No locations require validation.")
   }
