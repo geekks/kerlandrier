e38d5223c67016e4620a80c47a2ce33c8fbec9f4
adrzcn <adrien.cousin@lilo.org>
2024-09-29 20:25:52 +0200

    Prompt user to select existing location

--- ics/import_ics.js
+++ ics/import_ics.js
@@ -8,6 +8,9 @@ const fs = require('fs');
 
 const { pullUpcomingIcsEvents, createOaEvent, updateOaEvent, patchOaEvent } = require("../utils")
 const { getCorrespondingOaLocation } = require("../resources/getOaLocation")
+const { retrieveAccessToken, getLocations } = require("../resources/manualHttpRequests")
+
+const TBD_LOCATION_UID = process.env.TBD_LOCATION_UID 
 
 // OpenAgenda creds
 const publicKey = process.env.OA_PUBLIC_KEY;
@@ -27,16 +30,40 @@ const importIcs = async (icsUrl) => {
   const newOaEvents = await pullUpcomingIcsEvents(icsUrl)
   console.log("icsUrl - ", icsUrl);
 
+// Build location prompt
+  const accessToken = await retrieveAccessToken(secretKey)
+    const locations = await getLocations(accessToken)
+    // Sort by city and address
+    locations.sort((a, b) => {
+      if (a.city < b.city) return -1
+      if (a.city > b.city) return 1
+      if (a.name < b.name) return -1
+      if (a.name > b.name) return 1
+      return 0
+    })
+    // console.log("locations - ", locations);
+    const locationsChoices = locations.map(l => {
+      return {name: `${l.city} - ${l.name} - ${l.address}`, value: l.uid}
+    });
+locationsChoices.unshift({name: "=== DEFAULT LOCATION ===",value: TBD_LOCATION_UID})
+    const locationsQuestion = {
+          type: 'list',
+          name: 'uid',
+          message: 'Select the location corresponding to the event',
+        choices: locationsChoices
+    }
+
+
   // Iterate over upcoming events
   const uids = []
   let i = 0;
   for (const newOaEvent of newOaEvents) {
-    console.log("\n======== Scanning newOaEvent ========");
+    // console.log("\n======== Scanning newOaEvent ========");
     // // Skip guessing location for now
     // const locationResult = await getCorrespondingOaLocation(newOaEvent.locationName)
     // console.log("locationResult - ", locationResult);
     // if (locationResult) newOaEvent.locationUid = locationResult
-    console.log(newOaEvent.title, newOaEvent.timings[0].begin, newOaEvent.locationName);
+    // console.log(newOaEvent.title, newOaEvent.timings[0].begin, newOaEvent.locationName);
     try {
       // For each upcoming event, check if it already exists in Open Agenda
       // The key is the "uid-externe" but the API does not allow to filter on it
@@ -50,10 +77,10 @@ const importIcs = async (icsUrl) => {
           headers: { 'Content-Type': 'application/json' }
         }
       )
-      // Check if an existing oa event is there with the same "uid-externe"
+      // Find existing oa event with same "uid-externe"
       const oaEvent = oaEvents.data.events.find(event => event["uid-externe"] === newOaEvent["uid-externe"]);
       if (!oaEvent && oaEvents && oaEvents.data.events.length > 0) {
-        // Prompt the user to select an existing event or create a new one
+        // No existing event, prompt the user to select an existing event or create a new one
         const oaEventsChoices = oaEvents.data.events.map((e) => {
           return {
             name: `${e.title} - ${e.location.name}`,
@@ -63,34 +90,42 @@ const importIcs = async (icsUrl) => {
 
         oaEventsChoices.unshift({name: "Créer un nouvel événement",value: null})
 
-        const question = {
-          type: 'list',
-          name: 'uid',
-          message: 'Select the OA event corresponding to the GA event',
-          choices: oaEventsChoices
-        }
-
-        // const answer = await inquirer.prompt(question)
+        // Do this to auto-create new event
         const answer = null
-
+        
+        // // Do this to ask user
+        // const question = {
+        //   type: 'list',
+        //   name: 'uid',
+        //   message: 'Select the OA event corresponding to the GA event',
+        //   choices: oaEventsChoices
+        // }
+        // const answer = await inquirer.prompt(question) 
+        
         if (!answer || answer.uid === null) {
-          // No existing event, create a new one
-          console.log("\t[CREATE] Create new event")
+          
+          // New oa event auto-creation or
+          // User asks to create a new oa event
+          console.log("\t[CREATE] Create new event - User decision")
+          // Ask for location via user prompt
+          console.log("newOaEvent - ", newOaEvent.title, newOaEvent.description.fr, newOaEvent.timings[0].begin);
+         const location = await inquirer.prompt(locationsQuestion)
+         if (location) newOaEvent.locationUid = location.uid
           const createdEvent = await createOaEvent(oa, AGENDA_UID, newOaEvent)
           console.log("\t[CREATE] uid", createdEvent.uid);
           uids.push(createdEvent.uid)
         } else {
-          // Patch externe-uid to the oa event
-          console.log("\t[PATCH] Patch existing event - ", oaEventsChoices.find((e) => e.value === answer.uid).name);
-        const patchData = {"uid-externe": newOaEvent["uid-externe"]}
+          // User has selected an existing oa event
+        //   console.log("\t[PATCH] Patch existing event - ", oaEventsChoices.find((e) => e.value === answer.uid).name);
+        // const patchData = {"uid-externe": newOaEvent["uid-externe"]}
           const patchedEvent = await patchOaEvent(oa, AGENDA_UID, answer.uid, patchData)
           console.log("\t[PATCH] uid", patchedEvent.uid);
           // Do not add uid to the log list to avoid deleting an event that's not originally from the ics file
         }
       } else if (oaEvent) {
         // Option 1: SIMPLE - Existing event, do nothing. We trust first import only
-        console.log("\t[SKIP] Skip existing event - ", oaEvent.title);
-        uids.push(oaEvent.uid)
+        // console.log("\t[SKIP] Skip existing event - ", oaEvent.title);
+        // Do not add uid to the log list to avoid deleting an event by mistake
         //  // Option 2: RISKY - Existing event, update it in case it has changed since creation. We trust ics live source.
         // console.log("\t[UPDATE] Update existing event - ", oaEvent.title);
         // const updatedEvent = await updateOaEvent(oa, AGENDA_UID, oaEvent.uid, newOaEvent)
@@ -98,23 +133,27 @@ const importIcs = async (icsUrl) => {
         // uids.push(updatedEvent.uid)
       } else {
         // Default: Create the event because nothing seems to be there
-        console.log("\t[CREATE] Create new event")
+        console.log("\t[CREATE] Create new event - Default")
+        // Ask for location via user prompt
+          console.log("newOaEvent - ", newOaEvent.title, newOaEvent.description.fr, newOaEvent.timings[0].begin);
+         const location = await inquirer.prompt(locationsQuestion)
+        if (location) newOaEvent.locationUid = location.uid
         const createdEvent = await createOaEvent(oa, AGENDA_UID, newOaEvent)
         console.log("\t[CREATE] uid", createdEvent.uid);
         uids.push(createdEvent.uid)
       }
     } catch (err) {
       if (err.response.data.errors) {
-        console.log("Error: ", err.response.data.errors);
+        console.log("OA Error: ", err.response.data.errors);
       } else {
-        console.log("Error: ", err);
+        console.log("Other Error: ", err);
       
       }
     } finally {
-      console.log("uids - ", uids) // Re-user ids for further update, delete...
+      // console.log("uids - ", uids) // Re-user ids for further update, delete...
       fs.writeFile(`ics/import_ics_${now}.txt`, uids.join('\n'), (err) => {
         if (err) throw err;
-        console.log('Log file saved.');
+        // console.log('Log file saved.');
       });
       i++
     }
