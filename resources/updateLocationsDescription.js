b7d5613ce457817ccd896f9ea3a83d7df5ef79ea
JB Bailly <jean.baptiste.bailly@gmail.com>
2024-11-20 15:31:50 +0100

    up: refactor and localize new locations

--- resources/updateLocationsDescription.js
+++ resources/updateLocationsDescription.js
@@ -13,8 +13,26 @@ const oa = new OaSdk({
   secretKey,
 });
 
+// Aven
+const avenCities=['Bannalec','Beg-Meil','Concarneau','Elliant','LaForêt-Fouesnant','Pleuven',
+  'Pont-Aven','Rosporden','Fouesnant','Melgven','Moelansurmer','Moëlan-sur-Mer',
+  'Kervaziou','Scaër','Névez','Nizon','Port-la-Forêt','Quimperlé','Saint-Philibert',
+  'Saint-Yvi','Tourch','Trégunc','La Forêt-Fouesnant',
+  'Autre' // Pour les lieux inconnus, localisés par défaut à BILBAO
+];
+
+const cornouailleCities=['Aber-Wrac\'h','Quimper','Pont-l\'Abbé','Briec','Douarnenez' ,'Penmarc\'h','Lechiagat',
+  'Léchiagat','Ergué Gaberic','Ergué-Gabéric','Chateaulin','Châteaulin','Plobannalec',
+  'Plobannalec-Lesconil','Pluguffan','Trégornan','Combrit','Île-Tudy','Saint-Goazec',
+  'Saint-Brieuc','Plomelin','Clohars-Carnoët'];
+
+const bretagneCities= ['Rennes','Malestroit','Quéven','Malguénac','Landerneau','Mellionnec','Brest',
+  'Bréal-sous-Montfort','Pont-Scorff','Ploemeur','Plœmeur','Guingamp','Baud','Lorient'];
+
+// TO DO: lowercase + slugify location name for better matching
 
 // https://fr.wikipedia.org/wiki/Pays_de_Bretagne#/media/Fichier:Pays_Bretagne_map.jpg
+// http://www.heritaj.bzh/website/image/ir.attachment/4925_2e00c37/datas
 (async () => {
   const locationsUrls = []
   await oa.connect();
@@ -25,64 +43,24 @@ const oa = new OaSdk({
     for (const location of locations) {
       if (!location.description) console.log("!!! NO DESCRIPTION !!!", location.name, location.city)
       locationsUrls.push(`"https://openagenda.com/kerlandrier/admin/locations/${location.uid}/edit",`)
-      if (location.city === 'Bannalec' ||
-      // AVEN
-       location.city === 'Beg-Meil' ||
-       location.city === 'Concarneau' ||
-       location.city === 'Elliant' ||
-       location.city === 'La Forêt-Fouesnant' ||
-       location.city === 'Pleuven'  ||
-       location.city === 'Pont-Aven' ||
-       location.city === 'Rosporden' ||
-       location.city === 'Fouesnant' ||
-       location.city === 'Melgven' ||
-       location.city === 'Névez' ||
-       location.city === 'Nizon' ||
-       location.city === 'Port-la-Forêt' ||
-       location.city === 'Quimperlé' ||
-       location.city === 'Saint-Philibert' ||
-       location.city === 'Saint-Yvi' ||
-       location.city === 'Tourch' ||
-       location.city === 'Trégunc'
-      ) {
-        await oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "AVEN"} })
-      } else if (location.city === 'Aber-Wrac\'h' ||
-      // CORNOUAILLE
-      location.city === 'Quimper' ||
-      location.city === 'Pont-l\'Abbé' ||
-      location.city === 'Briec' ||
-      location.city === 'Douarnenez'  ||
-      location.city === 'Penmarc\'h' ||
-      location.city === 'Lechiagat' ||
-      location.city === 'Léchiagat' ||
-      location.city === 'Ergué Gaberic' ||
-      location.city === 'Ergué-Gabéric' ||
-      location.city === 'Chateaulin' ||
-      location.city === 'Châteaulin' ||
-      location.city === 'Plobannalec' ||
-      location.city === 'Plobannalec-Lesconil' ||
-      location.city === 'Pluguffan'
-    ) {
-      await oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "CORNOUAILLE"} })
-    } else if (location.city === 'Rennes' ||
-    // BRETAGNE
-    location.city === 'Malestroit' ||
-    location.city === 'Quéven' ||
-    location.city === 'Malguénac' ||
-    location.city === 'Landerneau' ||
-    location.city === 'Mellionnec' ||
-    location.city === 'Brest' ||
-    location.city === 'Bréal-sous-Montfort' ||
-    location.city === 'La Rochelle' ||
-    location.city === 'Lorient') {
-      await oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "BRETAGNE"} })
-      } else {
-        console.log("No update", location.name, location.address)
+      if      (avenCities.includes(location.city)) await 
+          oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "AVEN"} })
+      else if (cornouailleCities.includes(location.city)) await 
+          oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "CORNOUAILLE"} })
+      else if (bretagneCities.includes(location.city)) await 
+          oa.locations.patch(AGENDA_UID, location.uid, { description: {fr: "BRETAGNE"} })
+      else {
+        console.log("No update" +" / "+ location.name + 
+          " / "+ location.address +
+          " / "+ location.city +
+          " / "+ JSON.stringify(location.description) 
+        )
       }
     }
-    fs.writeFile('./resources/updateLocationsDescription.txt', "[" + locationsUrls.join('\n') + "]", function (err) {
-      if (err) return console.log(err)
-    })
+    fs.writeFile(file='./resources/updateLocationsDescription.txt', 
+                callback = "[" + locationsUrls.join('\n') + "]",
+                function (err) {if (err) return console.log(err)}
+              )
   } else {
     console.log("No locations.")
   }
